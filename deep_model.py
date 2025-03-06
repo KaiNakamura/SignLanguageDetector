8b6291dfaca295ca8f9b27cec6dc42efefc69d7d
Karish Gupta <karishgupta2003@gmail.com>
2025-03-05 22:46:39 -0500

    Baseline implemnted, ensamble model implemented, and minor updates done

--- deep_model.py
+++ deep_model.py
@@ -13,19 +13,15 @@ class ConvolutionalNeuralNetwork(nn.Module):
     def __init__(self, num_classes: int, num_dense_nodes: int):
         super(ConvolutionalNeuralNetwork, self).__init__()
         self.conv_layer = nn.Sequential(
-            nn.Conv2d(
-                in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1
-            ),
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(
-                in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1
-            ),
+
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(
-                in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1
-            ),
+            
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
